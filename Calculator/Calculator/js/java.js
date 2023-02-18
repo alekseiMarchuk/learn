73d626e7cec1fd0e8543badb6abc3b4c38a103f9
alex <alekseymarchuck@gmail.com>
2023-02-18 12:17:08 +0300

    addet file

---
+++ Calculator/Calculator/js/java.js
@@ -0,0 +1,94 @@
+
+
+
+if(document.querySelector("#buttonOne").onclick = true){
+console.log(1212);
+}
+
+
+
+
+
+
+
+
+
+
+function Write1(numb){
+
+    // a = 0
+    // b = 0
+    // c = " "
+    // d = 0
+    // e = " "
+
+    // if(numb == " + "){
+    //     c = numb
+    // }
+    // if(c == " "){
+    //     document.querySelector('.firstNumber').innerHTML += numb; 
+    // }
+    // if(c !== " "){
+    //     a = document.querySelector('.firstNumber').innerHTML
+
+    //     document.querySelector('.mathematicalSign').innerHTML += numb; 
+    //    return numb;
+    // }
+
+    // console.log(a)
+
+    // if(d == " = "){
+    //     a = String(a);
+    //     numb = String(numb);
+
+
+    // }
+
+    // console.log(a)
+    // console.log(numb)
+}
+
+
+
+
+
+
+
+// if(c == " + "){
+// d = a + b
+// }if(c == " - "){
+// d = a - b
+// }if(c == " * "){
+// d = a * b
+// }if(c == " / "){
+// d = a / b
+// }
+
+
+
+// a = document.querySelector('.firstNumber').innerHTML
+
+// b = document.querySelector('.secondNumber').innerHTML
+
+// c = document.querySelector('.mathematicalSign').innerHTML
+
+// d = document.querySelector('.thisIs').innerHTML
+
+
+
+
+
+
+
+
+
+// function Write1(numb){
+//     document.querySelector('.firstNumber').innerHTML += numb; 
+// }
+// function ItIs(that){
+//     a = document.querySelector('.firstNumber').innerHTML
+//     a = String(a);
+//     document.querySelector('.firstNumber').innerHTML += " = "
+// }
+
+
